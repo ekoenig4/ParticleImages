5690c24d156ee551fbcf333a10e32e97803cf057
bear-is-asleep <67704640+bear-is-asleep@users.noreply.github.com>
2021-11-21 22:30:29 -0500

    New updates

--- utils/helpers.py
+++ utils/helpers.py
@@ -7,6 +7,7 @@ from matplotlib import animation
 import os
 %matplotlib qt
 
+<<<<<<< Updated upstream
 def animate(X,y,t_bins,images=range(-1,1),interval=500):
     """
     Animate images, from -5 to 5 (es and photons)
@@ -28,6 +29,20 @@ def animate(X,y,t_bins,images=range(-1,1),interval=500):
         plt.close()
     os.system('mv *.gif gifs/')
     plt.close()
+=======
+from keras.layers import Conv2D, MaxPooling2D,AveragePooling2D,Conv3D, MaxPooling3D,AveragePooling3D,concatenate
+
+img_rows, img_cols, nb_channels = 32, 32, 2
+input_dir = 'data'
+decays = ['SinglePhotonPt50_IMGCROPS_n249k_RHv1',
+          'SingleElectronPt50_IMGCROPS_n249k_RHv1']
+channelMap = {0: 'Energy', 1: 'Time'}
+decayMap = {0: 'Photon', 1: 'Electron'}
+
+
+def load_data(start, stop):
+    """Load photon and electron data from data/ directory
+>>>>>>> Stashed changes
 
 img_rows, img_cols, nb_channels = 32, 32, 2        
 input_dir = 'data'
@@ -75,16 +90,24 @@ def timeordered_BC(X,cumulative=False,remove_empty=True,min_t = -0.05,max_t = 0.
     """
     if remove_empty:
         X = remove_empty_pixels(X)
+<<<<<<< Updated upstream
     X_e,X_t = X[:,:,:,0],X[:,:,:,1] #Decompose energy and time
     n_images,width,height,channels = X.shape #Find shape of images
     t_bins = np.arange(min_t, max_t, t_step) #Bin separation for images
     print(t_bins)
     t_mats = [np.full(shape=(width,height),fill_value=t) for t in t_bins]
+=======
+    X_e, X_t = X[:, :, :, 0], X[:, :, :, 1]  # Decompose energy and time
+    n_images, width, height, channels = X.shape  # Find shape of images
+    t_bins = np.arange(min_t, max_t, t_step)  # Bin separation for images
+    t_mats = [np.full(shape=(width, height), fill_value=t) for t in t_bins]
+>>>>>>> Stashed changes
     max_frames = len(t_mats)
     X_e_timeordered = np.zeros(shape=(n_images,max_frames,width,height))
     X_t_timeordered = np.zeros(shape=(n_images,max_frames,width,height))
     for i in range(n_images):
         for t in range(max_frames-1):
+<<<<<<< Updated upstream
             lower = X_train[i,:,:,1] > t_mats[t] #Lower bound
             upper = X_train[i,:,:,1] <= t_mats[t+1] #Upper bound
             is_between = np.logical_and(lower,upper) #Between upper and lower
@@ -92,6 +115,145 @@ def timeordered_BC(X,cumulative=False,remove_empty=True,min_t = -0.05,max_t = 0.
             X_t_timeordered[i,t,:,:] = np.where(~is_between,np.nan,X_t[i,:,:])
     
     return X_e_timeordered,X_t_timeordered,max_frames,t_bins
+=======
+            lower = X[i, :, :, 1] > t_mats[t]  # Lower bound
+            upper = X[i, :, :, 1] <= t_mats[t+1]  # Upper bound
+
+            if cumulative:
+                is_between = upper
+            else:
+                # Between upper and lower
+                is_between = np.logical_and(lower, upper)
+
+            X_e_timeordered[i, t, :, :] = np.where(
+                ~is_between, np.nan, X_e[i, :, :])
+            X_t_timeordered[i, t, :, :] = np.where(
+                ~is_between, np.nan, X_t[i, :, :])
+
+    if remove_empty:
+        X_e_timeordered = np.where(
+            np.isnan(X_e_timeordered), 0, X_e_timeordered)
+
+    return X_e_timeordered, X_t_timeordered, max_frames, t_bins
+
+def inception2D(input,filter_sizes=[60,50,40]):
+  """
+  input: previous layer's output
+  filter_sizes: sizes of kernels (1x1,2x2,3x3)
+  returns inception layer
+  """
+  #1x1 convolution
+  layer_1 = Conv2D(filters=filter_sizes[0], 
+                  activation='relu', 
+                  kernel_size=1,
+                  padding='same')(input)
+  #2x2 convolution
+  layer_2 = Conv2D(filters=filter_sizes[0], 
+                  activation='relu', 
+                  kernel_size=1,
+                  padding='same')(input)
+  layer_2 = Conv2D(filters=filter_sizes[1], 
+                  activation='relu', 
+                  kernel_size=2,
+                  padding='same')(layer_2)
+        
+  #3x3 convolution
+  layer_3 = Conv2D(filters=filter_sizes[0], 
+                  activation='relu', 
+                  kernel_size=1,
+                  padding='same')(input)
+  layer_3 = Conv2D(filters=filter_sizes[2], 
+                  activation='relu', 
+                  kernel_size=3,
+                  padding='same')(layer_3)
+  #2x2 max pooling
+  layer_4 = MaxPooling2D(pool_size=2,strides=1,
+                             padding='same')(input)
+  layer_4 = Conv2D(filters=filter_sizes[0], 
+                  activation='relu', 
+                  kernel_size=1,
+                  padding='same')(layer_4)
+
+  #mid_1 = concatenate([layer_1, layer_2, layer_3, layer_4], axis = 3)
+  mid_1 = concatenate([layer_2, layer_3, layer_4], axis = 3)
+  return mid_1
+
+def inception3D(input,filter_sizes=[30,20,10]):
+  """
+  input: previous layer's output
+  filter_sizes: sizes of kernels (1x1,2x2,3x3)
+  returns inception layer
+  """
+  #1x1 convolution
+  layer_1 = Conv3D(filters=filter_sizes[0], 
+                  activation='relu', 
+                  kernel_size=1,
+                  padding='same')(input)
+  #2x2 convolution
+  layer_2 = Conv3D(filters=filter_sizes[0], 
+                  activation='relu', 
+                  kernel_size=1,
+                  padding='same')(input)
+  layer_2 = Conv3D(filters=filter_sizes[1], 
+                  activation='relu', 
+                  kernel_size=2,
+                  padding='same')(layer_2)
+        
+  #3x3 convolution
+  layer_3 = Conv3D(filters=filter_sizes[0], 
+                  activation='relu', 
+                  kernel_size=1,
+                  padding='same')(input)
+  layer_3 = Conv3D(filters=filter_sizes[2], 
+                  activation='relu', 
+                  kernel_size=3,
+                  padding='same')(layer_3)
+  #2x2 max pooling
+  layer_4 = MaxPooling3D(pool_size=2,strides=1,
+                             padding='same')(input)
+  layer_4 = Conv3D(filters=filter_sizes[0], 
+                  activation='relu', 
+                  kernel_size=1,
+                  padding='same')(layer_4)
+
+  #mid_1 = concatenate([layer_1, layer_2, layer_3, layer_4], axis = 3)
+  mid_1 = concatenate([layer_2, layer_3, layer_4], axis = 4)
+  return mid_1
+
+
+def animate(X, y, t_bins, images=range(-1, 1), interval=500):
+    """
+    Animate images, from -5 to 5 (es and photons)
+    """
+    for i in images:
+        img = []
+        fig = plt.figure()
+        if y[i] == 1:
+            fig.suptitle('Electron')
+        else:
+            fig.suptitle('Photon')
+        for j in range(X.shape[1]-1):
+            label = f'{t_bins[j]:.2f} < t < {t_bins[j+1]:.2f}'
+            temp_img = X[i, j, :, :]
+            img.append([plt.imshow(temp_img, animated=True),
+                       plt.text(18, 28, label)])
+        ani = animation.ArtistAnimation(
+            fig, img, interval=interval, blit=True, repeat_delay=0)
+        plt.show()
+        ani.save(f'Energy_{i+abs(images[0])}.gif')
+        plt.close()
+    os.system('mv *.gif gifs/')
+    plt.close()
+
+def inline_animation(X,y,tbins,event=0,interval=500,**kwargs):
+    decay = decayMap[y[event]]
+    fig, ax = plt.subplots()
+    frames = [
+        [ax.imshow( np.where(frame==0,np.nan,frame) ),ax.text(1,1,f"{decay}: ({tlo:.2f},{thi:.2f})")] for frame,tlo,thi in zip(X[event],tbins[:-1],tbins[1:])
+    ]
+    ani = animation.ArtistAnimation(fig,frames,interval=interval,**kwargs)
+    return ani
+>>>>>>> Stashed changes
 
 
 def plot_event(X,y,event=0,channel=-1):
